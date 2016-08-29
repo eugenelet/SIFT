92e9ae5415d3c3d5fa6045701a1bac91e6ef9d7c
eugenelet <eugeneleeeutzuan@gmail.com>
2016-08-29 13:46:47 +0800

    blurimg txt generation update

--- src/dumpImage.cpp
+++ src/dumpImage.cpp
@@ -14,19 +14,25 @@ void dumpImage(mySIFT& image)
 
 
 	fout.close();
-
-	char* answer_file = "blurredImgs.txt";
-	fout.open(answer_file);
-
+	char answer_buf[80];
+	char* answer_file = "blurredImgs";
+	char* answer_end  = ".txt";
 	for(int i = 1; i < 5; i++){
+		strcpy(answer_buf, answer_file);
+		string tmp = to_string(i);
+		strcat(answer_buf, tmp.c_str());
+		strcat(answer_buf, answer_end);
+		fout.open(answer_buf);
+
 		for(int row = 0; row < image.blurredImgs[i].rows; row++){
 			for(int col = 0; col < image.blurredImgs[i].cols; col++)
 				fout << (unsigned int)image.blurredImgs[i].at<uchar>(row, col) << ' ' ;
 			fout << endl;
 		}
 		fout << endl;
+		fout.close();
+
 	}
 
-	fout.close();
 
 }
